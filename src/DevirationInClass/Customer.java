50d2f5e83e15be8e58c17717c1baf237e5d528e3
zurozira <cyrildebui@gmail.com>
2026-01-28 19:25:09 -0600

    Update Deviration in Class

---
+++ src/DevirationInClass/Customer.java
@@ -0,0 +1,39 @@
+package DevirationInClass;
+
+/**
+ * Customer class inherits Person class and add customer ID component
+ * @author Vu Cong Bui
+ */
+public class Customer extends Person {
+
+    private int customerID;
+
+    public Customer() {
+    }
+
+    public Customer(int customerID, String firstName, String lastName, char middleInit) {
+
+        super(firstName, lastName, middleInit);
+
+        this.customerID = customerID;
+    }
+
+    public int getCustomerID() { return customerID; }
+
+    public void setCustomerID(int customerID) { this.customerID = customerID; }
+
+    public String writeAsRecord() {
+
+        return String.format("%d %s %c %s", customerID, getFirstName(), getMiddleInit(), getLastName());
+    }
+
+    public String toString() {
+
+        return String.format("%d %s", customerID, super.toString());
+    }
+
+    public String getFullName() {
+
+        return super.toString();
+    }
+}
--- src/DevirationInClass/Person.java
+++ src/DevirationInClass/Person.java
@@ -1,35 +1,39 @@
 package DevirationInClass;
 
+/**
+ * Represent a person with full name components (First name, Last name, Middle initial)
+ * @author Vu Cong Bui
+ */
 public class Person {
 
     private String firstName;
     private String lastName;
-    protected String dob; // Protected -> Child can access
+    private char middleInit;
 
     public Person() {
 
     }
 
-    public Person(String firstName, String lastName, String dob) {
+    public Person(String firstName, String lastName, char middleInit) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.dob = dob;
+        this.middleInit = middleInit;
     }
 
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
-    public String getDOB() { return dob; }
+    public char getMiddleInit() { return middleInit; }
 
     public void setFirstName(String firstname) { this.firstName = firstname; }
     public void setLastName(String lastName) { this.lastName = lastName; }
-    public void setDOB(String dob) { this.dob = dob; }
+    public void setMiddleInit(char middleInit) { this.middleInit = middleInit; }
 
     /**
      *
-     * @return
+     * @return person's full name as formatted
      */
     @Override
     public String toString() {
-        return String.format("%s, %s", lastName, firstName);
+        return String.format("%s, %s %c", lastName, firstName, middleInit);
     }
 }
