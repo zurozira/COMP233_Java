1809f31c2b0a8cb9c05d44d59cd74e0778f9b9af
zurozira <cyrildebui@gmail.com>
2026-03-11 20:54:48 -0600

    Update Threads

--- src/NcDonalds/NcDonalds.java
+++ src/NcDonalds/NcDonalds.java
@@ -11,6 +11,7 @@ import java.util.concurrent.*;
  * Masterchef order first,
  * not Masterchef and not Slacker order second,
  * Slacker order last.
+ * @author Vu Cong Bui
  */
 public class NcDonalds {
 
